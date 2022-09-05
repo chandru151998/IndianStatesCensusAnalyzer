b392e04598ed17c1a66ae3105f29572285a7ec45
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-09-05 15:11:54 +0530

    UC1.2 Test

--- Tests.cs
+++ Tests.cs
@@ -8,8 +8,8 @@ namespace IndianStateCensusAnalyserTest
     {
 
         
-        static string indiaStateCensusData = @"C:\Users\tusha\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\CSV_Files\IndiaStateCensusData.csv";
-        static string wrongIndiaStateCensusData = @"C:\Users\tusha\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\CSV Files\WrongIndiaStateCensusData.csv";
+        static string indiaStateCensusData = @"C:\Users\Chandru\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\CSV_Files\IndiaStateCensusData.csv";
+        static string wrongIndiaStateCensusData = @"C:\Users\Chandru\source\repos\IndianStateCensusAnalyser\IndianStateCensusAnalyser\CSV Files\WrongIndiaStateCensusData.csv";
 
         static string indianStateCensusHeaders = "State,Population,AreaInSqKm,DensityPerSqKm";
 
